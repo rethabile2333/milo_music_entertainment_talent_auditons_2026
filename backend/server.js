2bed5bd99e40f7dddc806c01089050acd4436e21
rethabile2333 <rethabile.thatho@bothouniversity.com>
2026-08-04 23:36:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["http://localhost:3000", "https://milo-music-entertainment-talent-aud-mu.vercel.app"] }));
 app.use(express.json());
 
 app.use("/user", userRoutes);
