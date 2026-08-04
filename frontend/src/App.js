9c3f65a32c031a2c60ae1397cdf50be39171c2e6
rethabile2333 <rethabile.thatho@bothouniversity.com>
2026-08-04 17:37:35 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import UserDashboard from "./components/UserDashboard";
