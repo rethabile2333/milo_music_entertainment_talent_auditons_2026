d96866a7ac603210a0b8d4a08ccab8e8a0d517a8
rethabile2333 <rethabile.thatho@bothouniversity.com>
2026-08-05 12:37:55 +0200

    ye

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "milo_auditions"
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
 
 module.exports = db;
