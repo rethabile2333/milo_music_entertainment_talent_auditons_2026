e75ac328a69165d08adac2b1832c94fd0e5c54cb
rethabile2333 <rethabile.thatho@bothouniversity.com>
2026-08-05 15:24:55 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,21 +3,5 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://milo-music-entertainment-talent-auditons.onrender.com",
 });
-api.interceptors.request.use(
-    (config) => {
-
-        const token = localStorage.getItem("token");
-
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-
-    },
-    (error)=>{
-        return Promise.reject(error);
-    }
-);
 
 export default api;
